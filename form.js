6109bddfa5beb1ffe0f8c9cc42312663e60f33a0
ignatius omeje <ignatiusomeje153@gmail.com>
2024-10-30 08:31:40 +0100

    fix: working issue

--- form.js
+++ form.js
@@ -1,7 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-storage.js";
-import { classifyWasteType } from './wasteMapping.js';
+import {
+  getFirestore,
+  collection,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-storage.js";
+import { classifyWasteType } from "./wasteMapping.js";
 
 // Firebase configuration
 // form.js
@@ -10,19 +19,20 @@ const apiKey = import.meta.env.VITE_API_KEY;
 // Use the variable as needed
 // For example, in your Firebase config
 const firebaseConfig = {
-    apiKey: apiKey,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID,
-    measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  apiKey: apiKey,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
+
+const wasteType = document.getElementById("waste-type");
 
 // Initialize location input and suggestions
 const locationInput = document.getElementById("location-name");
@@ -30,27 +40,29 @@ const suggestionsList = document.getElementById("location-suggestions");
 const geocoder = L.Control.Geocoder.nominatim();
 
 locationInput.addEventListener("input", function () {
-    const query = locationInput.value;
-    if (query.length > 2) {
-        geocoder.geocode(query, function (results) {
-            suggestionsList.innerHTML = ""; // Clear previous suggestions
-            results.forEach(function (result) {
-                const li = document.createElement("li");
-                li.textContent = result.name;
-                li.addEventListener("click", function () {
-                    document.getElementById("latitude").value = result.center.lat;
-                    document.getElementById("longitude").value = result.center.lng;
-                    locationInput.value = result.name;
-                    suggestionsList.innerHTML = "";
-                });
-                suggestionsList.appendChild(li);
-            });
+  const query = locationInput.value;
+  if (query.length > 2) {
+    geocoder.geocode(query, function (results) {
+      suggestionsList.innerHTML = ""; // Clear previous suggestions
+      results.forEach(function (result) {
+        const li = document.createElement("li");
+        li.textContent = result.name;
+        li.addEventListener("click", function () {
+          document.getElementById("latitude").value = result.center.lat;
+          document.getElementById("longitude").value = result.center.lng;
+          locationInput.value = result.name;
+          suggestionsList.innerHTML = "";
         });
-    }
+        suggestionsList.appendChild(li);
+      });
+    });
+  }
 });
 
 // Handle form submission
-document.getElementById("submit-button").addEventListener("click", function () {
+document
+  .getElementById("submit-button")
+  .addEventListener("click", function () {
     const name = document.getElementById("location-name").value;
     const address = document.getElementById("address").value;
     const wasteType = document.getElementById("waste-type").value;
@@ -59,79 +71,134 @@ document.getElementById("submit-button").addEventListener("click", function () {
     const latitude = document.getElementById("latitude").value;
     const longitude = document.getElementById("longitude").value;
 
-    if (name && wasteType && address && description && imageFile && latitude && longitude) {
-        uploadData(name, address, wasteType, description, imageFile, latitude, longitude);
+    if (
+      name &&
+      wasteType &&
+      address &&
+      description &&
+      imageFile &&
+      latitude &&
+      longitude
+    ) {
+      uploadData(
+        name,
+        address,
+        wasteType,
+        description,
+        imageFile,
+        latitude,
+        longitude
+      );
     } else {
-        alert("Please fill in all fields and select a location.");
+      alert("Please fill in all fields and select a location.");
     }
-});
+  });
 
 // Upload data to Firebase
-function uploadData(name, address, wasteType, description, imageFile, latitude, longitude) {
-    const storageRef = ref(storage, 'images/' + imageFile.name);
-    uploadBytes(storageRef, imageFile).then((snapshot) => {
-        getDownloadURL(snapshot.ref).then((downloadURL) => {
-            addDoc(collection(db, "dumpingSites"), {
-                name: name,
-                address: address,
-                wasteType: wasteType,
-                description: description,
-                imageUrl: downloadURL,
-                latitude: parseFloat(latitude),
-                longitude: parseFloat(longitude)
-            })
-            .then(() => {
-                alert("Location submitted successfully!");
-                window.location.href = 'index.html';
-            })
-            .catch((error) => {
-                console.error("Error storing location:", error);
-                alert("An error occurred while storing the location.");
-            });
+function uploadData(
+  name,
+  address,
+  wasteType,
+  description,
+  imageFile,
+  latitude,
+  longitude
+) {
+  const storageRef = ref(storage, "images/" + imageFile.name);
+  uploadBytes(storageRef, imageFile).then((snapshot) => {
+    getDownloadURL(snapshot.ref).then((downloadURL) => {
+      addDoc(collection(db, "dumpingSites"), {
+        name: name,
+        address: address,
+        wasteType: wasteType,
+        description: description,
+        imageUrl: downloadURL,
+        latitude: parseFloat(latitude),
+        longitude: parseFloat(longitude),
+      })
+        .then(() => {
+          alert("Location submitted successfully!");
+          window.location.href = "index.html";
+        })
+        .catch((error) => {
+          console.error("Error storing location:", error);
+          alert("An error occurred while storing the location.");
         });
     });
+  });
 }
 
-// AI Waste Sorting with TensorFlow.js
+const URL = "./js/";
+
 let model;
-async function loadModel() {
-    model = await cocoSsd.load();
-    console.log("Model loaded.");
+
+async function init() {
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
+  model = await tmImage.load(modelURL, metadataURL);
 }
 
-document.getElementById("image-upload").addEventListener("change", async function (event) {
+document
+  .getElementById("image-upload")
+  .addEventListener("change", async function (event) {
     const feedbackDiv = document.getElementById("waste-type-feedback");
     feedbackDiv.textContent = "Analyzing waste type...";
-    feedbackDiv.style.display = 'block';
+    feedbackDiv.style.display = "block";
 
     const file = event.target.files[0];
     if (!file) {
-        console.error("No file selected for analysis.");
-        return;
+      console.error("No file selected for analysis.");
+      return;
     }
 
     const reader = new FileReader();
+    reader.readAsDataURL(file);
     reader.onload = async function (e) {
-        const img = document.createElement("img");
-        img.src = e.target.result;
+      const img = document.createElement("img");
+      img.src = e.target.result;
 
-        // Wait for the model to be loaded before analyzing
-        if (!model) await loadModel();
+      // Wait for the model to be loaded before analyzing
+      if (!model) await init();
 
-        const predictions = await model.dete
-        ct(img);
+      const predictions = await model.predict(img);
 
-        // Filter predictions to find relevant waste types
-        let foundWasteType = "";
-        predictions.forEach(prediction => {
-            const mappedWasteType = classifyWasteType(prediction.class);
-            if (mappedWasteType) foundWasteType = mappedWasteType;
-        });
+      // // Filter predictions to find relevant waste types
+      let foundWasteType = "";
+      const newSorted = predictions.sort(
+        (prev, next) => next.probability - prev.probability
+      );
+      foundWasteType = newSorted[0].className;
+      wasteType.value = foundWasteType;
 
-        feedbackDiv.textContent = foundWasteType
-            ? `Detected waste type: ${foundWasteType}`
-            : "Waste type could not be determined";
+      feedbackDiv.textContent = foundWasteType
+        ? `Detected waste type: ${foundWasteType}`
+        : "Waste type could not be determined";
     };
 
-    reader.readAsDataURL(file);
-});
+    // [
+    //     {
+    //         "className": "PAPER",
+    //         "probability": 0.00030855328077450395
+    //     },
+    //     {
+    //         "className": "NYLON",
+    //         "probability": 0.028732571750879288
+    //     },
+    //     {
+    //         "className": "RUBBER",
+    //         "probability": 0.04151686280965805
+    //     },
+    //     {
+    //         "className": "BOTTLE",
+    //         "probability": 0.8093022108078003
+    //     },
+    //     {
+    //         "className": "PLASTIC",
+    //         "probability": 0.11970028281211853
+    //     },
+    //     {
+    //         "className": "TINS",
+    //         "probability": 0.00043954767170362175
+    //     }
+    // ]
+  });
